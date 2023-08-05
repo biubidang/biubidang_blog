99ca01c0cee67410f3203380bc83b7bc7d581c6a
biubidang <2814318648@qq.com>
2023-08-05 16:18:26 +0800

    新增分类表

---
+++ blog_framework/src/main/java/com/biubidang/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package com.biubidang.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.biubidang.domain.entity.Category;
+
+
+/**
+ * (Category)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-08-05 15:42:10
+ */
+public interface CategoryMapper extends BaseMapper<Category> {
+
+}
