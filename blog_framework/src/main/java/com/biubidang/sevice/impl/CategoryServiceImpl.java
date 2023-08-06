a33e3e48c21fd18524f489343a54d540a2f154e4
biubidang <2814318648@qq.com>
2023-08-06 17:07:21 +0800

    新增查询分类功能

--- blog_framework/src/main/java/com/biubidang/sevice/impl/CategoryServiceImpl.java
+++ blog_framework/src/main/java/com/biubidang/sevice/impl/CategoryServiceImpl.java
@@ -1,10 +1,22 @@
 package com.biubidang.sevice.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.biubidang.Utils.BeanCopyUtils;
+import com.biubidang.domain.ResponseResult;
+import com.biubidang.domain.entity.Article;
 import com.biubidang.domain.entity.Category;
+import com.biubidang.domain.vo.CategoryListVo;
 import com.biubidang.mapper.CategoryMapper;
+import com.biubidang.sevice.ArticleService;
 import com.biubidang.sevice.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * (Category)表服务实现类
@@ -15,4 +27,26 @@ import org.springframework.stereotype.Service;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 
+
+    @Autowired
+    @Lazy
+    private CategoryService categoryService;
+    @Override
+    public ResponseResult getCategoryList() {
+        //目标：传入每个可用分类
+//        //获取所有已发表的文章
+//        LambdaQueryWrapper<Article> queryWrapper=new LambdaQueryWrapper<>();
+//        queryWrapper.eq(Article::getStatus,0);
+//        List<Article> articleList=articleService.list();
+        //传入分类必须可用
+        List<Category> categoryList=categoryService.list();
+        categoryList=categoryList.stream().
+                filter(category -> category.getStatus().equals("0"))
+                .collect(Collectors.toList());
+        //使用Vo封装Category信息
+        List<CategoryListVo> categoryListVo= BeanCopyUtils.copyBeanList(categoryList,CategoryListVo.class);
+        //返回统一格式
+        return ResponseResult.okResult(categoryListVo);
+
+    }
 }
