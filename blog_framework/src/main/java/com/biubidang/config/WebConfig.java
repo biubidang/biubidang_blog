ae7070a2603b5585349cd9b08c75d31987c762e5
“zzk” <2814318648@qq.com>
2023-09-24 21:35:07 +0800

    配置跨域

--- blog_framework/src/main/java/com/biubidang/config/WebConfig.java
+++ blog_framework/src/main/java/com/biubidang/config/WebConfig.java
@@ -8,11 +8,27 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许cookie
+                .allowCredentials(true)
+                // 设置允许的请求方式
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
     @Bean//使用@Bean注入fastJsonHttpMessageConvert
     //更改时间显示方式
     public HttpMessageConverter fastJsonHttpMessageConverters() {
